2dc14493c7b6ce495a936720298c9721a7c3a9c2
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-04-16 22:33:51 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Hitesh Madaan on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
