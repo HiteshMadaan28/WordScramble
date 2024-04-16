5f4141809e33d26eb15f1ad5f32ac1f3b989d8dd
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-04-16 23:39:00 +0530

    ..

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -16,7 +16,20 @@ struct ContentView: View {
             }
             Section("Dynamic Section")
             {
-                ForEach(0..<100)
+                ForEach(0..<3)
+                {
+                    Text("Hii \($0)")
+                }
+            }
+        }
+        List(0..<2){_ in
+            Section("This is the main Section")
+            {
+                Text("Hello is this Hitesh")
+            }
+            Section("Dynamic Section")
+            {
+                ForEach(0..<3)
                 {
                     Text("Hii \($0)")
                 }
